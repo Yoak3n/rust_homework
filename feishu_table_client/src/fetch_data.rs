0915bdebbcd7bf9ec908a3901256304e99255695
Yoake <3354590676@qq.com>
2024-01-22 18:41:56 +0800

    fix: fetch err

--- feishu_table_client/src/fetch_data.rs
+++ feishu_table_client/src/fetch_data.rs
@@ -28,9 +28,17 @@ pub fn new() -> DeviceInfo {
         // 获取系统信息
         let mut sys = sysinfo::System::new_all();
         sys.refresh_all();
-        let system_name = System::name().expect("Unknown OS");
-        self.device_type = system_name;
-        self.name = System::host_name().expect("Unknown Hostname");
+        if let Some(system_name) = System::name(){
+            self.device_type = system_name;
+        }else {
+            self.device_type = String::from("Unknown");
+        }
+        
+        if let Some(host_name) = System::host_name(){
+            self.name = host_name;
+        }else{
+            self.name = String::from("Unknown");
+        }
         self.memory_usage = get_memory_usage(&sys);
         self.cpu_usage = get_cpu_usage(&mut sys);
         self.disk_usage = get_disk_usage();
