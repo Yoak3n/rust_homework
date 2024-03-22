71b1222746e08426fd39e39f792127d7cf952526
Yoak3n <3354590676@qq.com>
2024-03-22 15:20:11 +0800

    24/3/22sc

--- privatebookmark/src/App.vue
+++ privatebookmark/src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import { ref, onMounted,computed, reactive } from 'vue'
+import { ref, onMounted, computed, reactive } from 'vue'
 import type { Category, Bookmark } from './api/type';
-import {read_bookmarks } from './api';
+import { read_bookmarks } from './api';
 import { useCategoryStore } from './store/modules/category';
 
 import ShowData from './components/common/ShowData.vue';
 import Drawer from './components/common/Drawer.vue';
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(true)
 const categoryStore = useCategoryStore()
 let activeTab = ref(0)
-let miniState = ref(false)
 const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
@@ -20,11 +19,11 @@ interface category {
 }
 let tabData: category[] = reactive([])
 
-const initializeCategories = async() => {
+const initializeCategories = async () => {
   await categoryStore.read_categories()
-  let res:Category[] = categoryStore.categories
+  let res: Category[] = categoryStore.categories
   console.log(res);
-  
+
   for (const item of res) {
     const c: category = {
       name: item.name,
@@ -35,26 +34,36 @@ const initializeCategories = async() => {
   }
 }
 
-onMounted(async() => {
+onMounted(async () => {
   await initializeCategories()
   read_bookmarks().then(res => {
     console.log(res);
     res.forEach(item => {
       if (item.category_id == undefined) {
         tabData[0].data.push(item)
-      }else{
+      } else {
         const index = tabData.findIndex(c => c.id == item.category_id)
         tabData[index].data.push(item)
       }
     })
     activeTab.value = tabData[tabData.length - 1].id
   })
+  // 防止右键菜单弹出
+  document.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    return false;
+  }, { capture: true })
+  document.addEventListener('selectstart', e => {
+    e.preventDefault();
+    return false;
+  }, { capture: true })
+
 })
 
 let data = computed(() => {
-  const  index = tabData.findIndex(c => c.id == activeTab.value)
+  const index = tabData.findIndex(c => c.id == activeTab.value)
   console.log(index);
-  if (index >= 0){
+  if (index >= 0) {
     return tabData[index].data
   }
   return []
@@ -66,7 +75,7 @@ let data = computed(() => {
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-white text-blue-grey-10" height-hint="98" >
+    <q-header elevated class="bg-white text-blue-grey-10" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -77,7 +86,7 @@ let data = computed(() => {
         <q-space />
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
       </q-toolbar>
-      <q-tabs align="left" v-model="activeTab" >
+      <q-tabs align="left" v-model="activeTab">
         <q-tab v-for="item in tabData" :key="item.id" :label="item.name" :name="item.id" />
       </q-tabs>
     </q-header>
@@ -86,24 +95,20 @@ let data = computed(() => {
     v-model="leftDrawerOpen" 
     side="right" 
     behavior="desktop" 
-    class="bg-grey-1" 
-    overlay 
-    bordered 
-    :width="150" 
-    :mini="miniState"
-    @mouseover="miniState = false"
-    @mouseout="miniState = true"
-    >
-      <Drawer/>
+    class="" 
+    max-height="100px"
+    max-width="200px"
+    overlay bordered mini
+    :width="150"
+      >
+      <Drawer />
     </q-drawer>
 
     <q-page-container>
-      <ShowData v-model:data="data"/>
+      <ShowData v-model:data="data" />
     </q-page-container>
   </q-layout>
 
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- privatebookmark/src/components/common/Drawer.vue
+++ privatebookmark/src/components/common/Drawer.vue
@@ -5,13 +5,13 @@
         <q-item-section avatar>
             <q-icon name="fa-regular fa-plus"/>
         </q-item-section>
-        <q-item-section >创建书签</q-item-section>
+        <q-tooltip class="text-body2" self="center right" anchor="center left" :offset="[10, 10]">创建书签</q-tooltip>
       </q-item>
       <q-item clickable @click="click2" v-ripple>
         <q-item-section avatar>
           <q-icon name="inbox" />
         </q-item-section>
-        <q-item-section>创建分类</q-item-section>
+        <q-tooltip class="text-body2" self="center right" anchor="center left" :offset="[10, 10]">创建分类</q-tooltip>
       </q-item>
       <!-- <q-item clickable @click="click2" v-ripple>
         <q-item-section avatar>
@@ -25,21 +25,19 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
           </q-avatar>
-
           <q-toolbar-title><span class="text-weight-bold">Create</span>Bookmark</q-toolbar-title>
-
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
 
         <q-card-section style="max-width: 400px">
-          <q-form class="q-gutter-md">
+          <q-form class="q-gutter-md" >
             <q-input filled label="书签名" v-model="data.name" />
             <q-input filled label="链接" v-model="data.url" />
             <q-input filled label="描述" v-model="data.content" />
             <q-select v-model="option" :options="categories" filled />
-            <div>
-              <q-btn label="Submit" @click="submit_bookmark"></q-btn>
-              <q-btn label="Submit" @click="click_category"></q-btn>
+            <div class="row items-center">
+              <q-btn class="col" label="Submit" @click="submit_bookmark"></q-btn>
+              <!-- <q-btn label="重置" @click="click_category"></q-btn> -->
             </div>
           </q-form>
         </q-card-section>
@@ -82,10 +80,10 @@ const categories = computed(() => {
     return { label: item.name, value: item.id }
   })
 })
-const click_category = () => {
-  let record: Bookmark = { name: data.name, url: computed_url.value, content: data.content, category_id: option.value.value }
-  console.log(record);
-}
+// const click_category = () => {
+//   let record: Bookmark = { name: data.name, url: computed_url.value, content: data.content, category_id: option.value.value }
+//   console.log(record);
+// }
 
 
 
