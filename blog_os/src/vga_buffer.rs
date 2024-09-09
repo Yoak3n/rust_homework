ea625b6259d88ea263714d53fa5508e1bfe12bba
Yoak3n <3354590676@qq.com>
2024-09-09 15:47:18 +0800

    feat: VGA text output

--- blog_os/src/vga_buffer.rs
+++ blog_os/src/vga_buffer.rs
@@ -19,3 +19,112 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+use volatile::Volatile;
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    row_postion:usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    fn new_line(&mut self) {
+        self.row_postion += 1;
+        self.column_position = 0;
+        if self.row_postion >= BUFFER_HEIGHT {
+            self.row_postion = BUFFER_HEIGHT - 1;
+            for row in 1..self.row_postion {
+                for col in 0..BUFFER_WIDTH {
+                    let character = self.buffer.chars[row][0].read();
+                    self.buffer.chars[row-1][col].write(character);
+                }
+
+            }
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = self.row_postion ;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(
+                    ScreenChar {
+                        ascii_character: byte,
+                        color_code,
+                    }
+                );
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+
+        }
+    }
+}
+
+
+pub fn print_something() {
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 10,
+        row_postion: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'\n');
+    writer.write_string("Hello ");
+    writer.write_string("World!\n");
+    write!(writer, "The numbers are {} and {}", 42, 1.11).unwrap();
+}
+
+
+use core::fmt;
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
